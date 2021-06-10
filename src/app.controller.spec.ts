1854f3525c68307b0d24d75156a2f256a8084f9b
Kim Yewon <kyw017763@gmail.com>
2021-06-10 23:36:07 +0900

    Fix test case

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Housework Contract Server!"', () => {
+      expect(appController.getHello()).toBe('Housework Contract Server!');
     });
   });
 });
