17c97067ef07cfc85cfc960e88027ea42d71a0e7
Kim Yewon <kyw017763@gmail.com>
2021-06-14 01:30:40 +0900

    Update after_deploy.sh, before_deploy.sh

--- after_deploy.sh
+++ after_deploy.sh
@@ -2,5 +2,5 @@ source /home/ec2-user/.bash_profile
 REPOSITORY=/home/ec2-user/housework-contract-server
 cd $REPOSITORY
 
-npm install -g pm2
+pm2 delete all
 pm2 start dist/main.js
--- before_deploy.sh
+++ before_deploy.sh
@@ -2,5 +2,4 @@ source /home/ec2-user/.bash_profileß
 REPOSITORY=/home/ec2-user
 cd $REPOSITORY
 
-pm2 delete all
 rm -rf housework-contract-server
