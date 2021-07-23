6eb79c899c22c0a6d0700fafdd2bdffaeb6297f7
Kim Yewon <kyw017763@gmail.com>
2021-07-24 01:08:31 +0900

    Change after_deploy.sh to simplify cp dest path

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/.bash_profile
 REPOSITORY=/home/ec2-user/housework-contract-server
-cp /home/ec2-user/.env.prod $REPOSITORY/.env.prod
+cp /home/ec2-user/.env.prod $REPOSITORY
 cd $REPOSITORY
 
 pm2 delete all
