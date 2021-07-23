2a87f55d1fce710e6f3d7dd8c8717ea78d838a33
Kim Yewon <kyw017763@gmail.com>
2021-07-24 01:26:57 +0900

    Fix after_deploy.sh

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/.bash_profile
 REPOSITORY=/home/ec2-user/housework-contract-server
-cp /home/ec2-user/.env.prod $REPOSITORY
+sudo cp /home/ec2-user/.env.prod $REPOSITORY
 cd $REPOSITORY
 
 pm2 delete all
