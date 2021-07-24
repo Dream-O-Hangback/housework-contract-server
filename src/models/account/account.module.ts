937886029923014b0c8fdce1ac1aea9e78baa0bb
Kim Yewon <kyw017763@gmail.com>
2021-07-25 06:06:29 +0900

    Change indentation -> 4

--- src/models/account/account.module.ts
+++ src/models/account/account.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import Account from '../account/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
-  controllers: [AccountController],
-  providers: [AccountService],
-  exports: [AccountService],
+    imports: [TypeOrmModule.forFeature([Account])],
+    controllers: [AccountController],
+    providers: [AccountService],
+    exports: [AccountService],
 })
 export class AccountModule {}
